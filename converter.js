ef9235c6f6b6dfa2bacddc0f5595c17e941c726d
mrmoon0986 <92893411+mrmoon0986@users.noreply.github.com>
2021-10-20 23:08:29 +0100

    Update converter.js

--- converter.js
+++ converter.js
@@ -1,24 +1,13 @@
-function csvJSON(csv){
+function convertor(csv) {
 
-  var lines=csv.split("\n");
+	const lines = csv.split('\n');
 
-  var result = [];
-
-  var headers=lines[0].split(".");
-
-  for(var i=1;i<lines.length;i++){
-
-	  var obj = {};
-	  var currentline=lines[i].split(".");
-
-	  for(var j=0;j<headers.length;j++){
-		  obj[headers[j]] = currentline[j];
-	  }
-
-	  result.push(obj);
-
-  }
-  
-  //return result; //JavaScript object
-  return JSON.stringify(result); //JSON
+	lines.map((el, i) => {
+	const obj = {};
+	if (i > 0) {
+		obj[lines[0].split('.')[0]] = el.split('.')[0];
+		obj[lines[0].split('.')[1].split('\r')[0]] = el.split('.')[1].split('\r')[0];
+		contactList.push(obj);
+	}
+	});
 }
