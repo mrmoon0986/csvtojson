c578c744217678ae0e22c7630289b55723b8a886
mrmoon0986 <92893411+mrmoon0986@users.noreply.github.com>
2021-10-20 23:02:15 +0100

    Update converter.js

--- converter.js
+++ converter.js
@@ -4,12 +4,12 @@ function csvJSON(csv){
 
   var result = [];
 
-  var headers=lines[0].split(",");
+  var headers=lines[0].split(".");
 
   for(var i=1;i<lines.length;i++){
 
 	  var obj = {};
-	  var currentline=lines[i].split(",");
+	  var currentline=lines[i].split(".");
 
 	  for(var j=0;j<headers.length;j++){
 		  obj[headers[j]] = currentline[j];
